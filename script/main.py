b5884b9d9a40a5b11dc881d509126dac7ecf4063
zhenliu26 <zhenliu26@outlook.com>
2021-05-19 11:32:16 -0400

    image shows

--- script/main.py
+++ script/main.py
@@ -2,20 +2,27 @@
 import sys
 from osgeo import gdal
 from matplotlib import pyplot as plt
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtCore import Qt, QUrl, QRect
 
 class Appdemo(QMainWindow):
     def __init__(self):
         super().__init__()
         self.resize(200, 200)
         self.setAcceptDrops(True)
+        self.label_1 = QLabel("Drop files here", self)
+        self.label_1.setAlignment(Qt.AlignCenter)
+        self.label_1.setStyleSheet("border:2px dashed #242424;")
+        self.label_1.setGeometry(QRect(10, 10, 180, 180))
     ## To check whether the item has url. If it has, the event will be accepted. If not, the event will be ignored.
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
             event.accept()
         else:
             event.ignore()
+    # def dragLeaveEvent(self, event):
+    #     if event.mimeData().hasUrls():
+    #         self.label_1.setHidden(False)
     # def dragMoveEvent(self, event):
     #     if event.mimeData().hasUrls():
     #         event.setDropAction(Qt.Copyction)
@@ -23,7 +30,8 @@ class Appdemo(QMainWindow):
     #     else:
     #         event.ignore()
     def dropEvent(self, event):
-        # event.setDropAction(Qt.Copyction)
+        ## close all figures
+        plt.close('all')
         event.accept()
         url_list = []
         for url in event.mimeData().urls():
@@ -33,9 +41,12 @@ class Appdemo(QMainWindow):
                 res_url = url.toString()
             url_list.append(res_url)
         print(url_list)
+        image_num = 1
         for url in url_list:
             img = gdal.Open(url)
             bandarray = img.GetRasterBand(1).ReadAsArray()
+            plt.figure(image_num)
+            image_num+=1
             fig = plt.gcf()
             fig.canvas.manager.set_window_title(url.split('/')[-1])
             plt.imshow(bandarray)
